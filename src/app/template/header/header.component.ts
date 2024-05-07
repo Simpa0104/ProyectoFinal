faa9d3cba12155afd9460cd8573d8de09205541a
Simpa0104 <Steventorres042001@gmail.com>
2024-05-07 02:53:24 -0500

    Se modifica visualmente muchas cosas xd

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  showNavbar: boolean = true;
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Verifica si la ruta actual es la de login
+    this.router.events.subscribe(event => {
+      if (this.router.url === '/login') {
+        this.showNavbar = false; // Oculta la barra de navegación en la página de login
+      } else {
+        this.showNavbar = true; // Muestra la barra de navegación en otras páginas
+      }
+    });
+  }
 }
