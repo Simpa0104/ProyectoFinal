bcaa3f1941e5e2d4cd9df86ab55d0968c9204794
Simpa0104 <Steventorres042001@gmail.com>
2024-05-18 11:45:35 -0500

    Se agrega algo al home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  showIndex: boolean = true;
   title = 'ProyectoFinal';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Verifica si la ruta actual es la de login
+    this.router.events.subscribe(event => {
+      if (this.router.url === '/login' || this.router.url === '/venta' || this.router.url === '/productos' || this.router.url === '/historial' || this.router.url === '/proveedores') {
+        this.showIndex = false; // Oculta la barra de navegación en la página de login
+      } else {
+        this.showIndex = true; // Muestra la barra de navegación en otras páginas
+      }
+    });
+  }
 }
